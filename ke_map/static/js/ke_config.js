695c594a80af0aae939307f964a479a78da53a2a
Kenneth Edwards <kennethedwards.1776@gmail.com>
2020-02-09 16:30:40 -0500

    adding MAP for Cumulative Delta

---
+++ ke_map/static/js/ke_config.js
@@ -0,0 +1,4 @@
+// for app.py config: postgres_key = 'postgresql//saba:saba123@http://127.0.0.1:49758/browser//crash'
+// API key
+const API_KEY = "pk.eyJ1Ijoia2VubmV0aC1lZHdhcmRzIiwiYSI6ImNrNDF3dzVleTA1YmszbnJ3YjJibGx1enoifQ.DaYX1rgmXxpQgfY2e8jIXQ" 
+
